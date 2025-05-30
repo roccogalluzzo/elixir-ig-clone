f9c3c6c4218f237f6b78acf17f0e496dddd064fb
Rocco Galluzzo <rocco.galluzzo@iubenda.com>
2025-05-30 13:08:54 +0200

    Update migration

--- priv/repo/migrations/20250523120000_create_posts.exs
+++ priv/repo/migrations/20250523120000_create_posts.exs
@@ -5,7 +5,7 @@ defmodule ElixirIgClone.Repo.Migrations.CreatePosts do
     create table(:posts) do
       add :title, :string, null: false
       add :description, :text, null: false
-      add :image, :string, null: false
+      add :image, :string
       add :user_id, references(:users, on_delete: :delete_all), null: false
       timestamps(type: :utc_datetime)
     end
